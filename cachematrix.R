a6fa56958c712e0185e2d69d8b053089c222a7dc
stevecox1964 <stevecoxiscool@gmail.com>
2015-04-25 20:30:17 -0500

    Coursera Week 3 home work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Example functions which show how to store 
+## potentially computation intensive procedures
+## into local function environment for quick
+## retrieval
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Function used to make a list of functions which
+## invert a matrix and cache the results
+makeCacheMatrix <- function(x = matrix()) 
+{
+        inv = NULL
+        set = function(y) 
+		{
+                x <<- y
+                inv <<- NULL
+        }
+		#function used to get matrix
+        get = function() x
+		#function used to set matrix
+        setinv = function(inverse) inv <<- inverse 
+        getinv = function() inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## function that calculates the inverse of a matrix 
+## using previously cached value if available which speeds
+## execution
+cacheSolve <- function(x, ...) 
+{
+        inv = x$getinv()
+        # if the inverse has already been calculated
+        if (!is.null(inv))
+		{
+                # return from function environment (cache) 
+				# and skip re-computation. 
+                return(inv)
+        }
+        
+        # else, calculate inverse and store in cache before returning
+		# value
+        mat.data = x$get()
+        inv = solve(mat.data, ...)
+        
+        # set the value of the inverse matrix into function (cache) environment
+        x$setinv(inv)
+        
+        return(inv)
 }
